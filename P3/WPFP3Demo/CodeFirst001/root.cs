4234c41e83b9af0c519544f2fc429b639db0f90a
guhuan769 <769540542@qq.com>
2023-08-30 11:42:51 +0800

    gg

---
+++ P3/WPFP3Demo/CodeFirst001/root.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst001
+{
+    public class root
+    {
+        public string a { get; set; }
+        public A aa { get; set; }
+    }
+    public class A {
+        public string b{ get; set; }
+    }
+}
